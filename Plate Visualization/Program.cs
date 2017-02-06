dbc218b6fd4994f370ae79727e89d4e67691954d
Ngo Nghia <ngonghiahp93@gmail.com>
2017-02-06 16:40:03 +0300

    Create main menu

--- Plate Visualization/Program.cs
+++ Plate Visualization/Program.cs
@@ -16,7 +16,7 @@ namespace Plate_Visualization
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
