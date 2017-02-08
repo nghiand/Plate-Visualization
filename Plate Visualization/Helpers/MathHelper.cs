f9da463295bf381713bfea50cb50f43e635afb56
Ngo Nghia <ngonghiahp93@gmail.com>
2017-02-08 15:30:19 +0300

    Add class MathHelper

---
+++ Plate Visualization/Helpers/MathHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plate_Visualization.Helpers
+{
+    class MathHelper
+    {
+        static double distance(Point p1, Point p2)
+        {
+            return Math.Sqrt(Math.Pow(p1.X - p2.X, 2) + Math.Pow(p1.Y - p2.Y, 2));
+        }
+    }
+}
