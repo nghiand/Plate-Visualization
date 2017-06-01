57313c6b2b09f9ea8f4ca0a9926d1bf11c46b2cd
nghiand <ngonghiahp93@gmail.com>
2017-06-01 15:59:47 +0300

    Add result form to show calculation result plate

---
+++ Plate Visualization/views/ResultForm.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace Plate_Visualization.views
+{
+    partial class ResultForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.resultPicturebox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.resultPicturebox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // resultPicturebox
+            // 
+            this.resultPicturebox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.resultPicturebox.Location = new System.Drawing.Point(0, 0);
+            this.resultPicturebox.Name = "resultPicturebox";
+            this.resultPicturebox.Size = new System.Drawing.Size(1036, 647);
+            this.resultPicturebox.TabIndex = 0;
+            this.resultPicturebox.TabStop = false;
+            this.resultPicturebox.SizeChanged += new System.EventHandler(this.resultPicturebox_SizeChanged);
+            this.resultPicturebox.Paint += new System.Windows.Forms.PaintEventHandler(this.resultPicturebox_Paint);
+            this.resultPicturebox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.resultPicturebox_MouseDown);
+            this.resultPicturebox.MouseMove += new System.Windows.Forms.MouseEventHandler(this.resultPicturebox_MouseMove);
+            this.resultPicturebox.MouseUp += new System.Windows.Forms.MouseEventHandler(this.resultPicturebox_MouseUp);
+            // 
+            // ResultForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1036, 647);
+            this.Controls.Add(this.resultPicturebox);
+            this.Name = "ResultForm";
+            this.ShowIcon = false;
+            this.Text = "Результат расчета";
+            this.Load += new System.EventHandler(this.ResultForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.resultPicturebox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox resultPicturebox;
+    }
+}
---
+++ Plate Visualization/views/ResultForm.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Plate_Visualization.views
+{
+    public partial class ResultForm : Form
+    {
+        private Plate result;
+        private Graphic graphic;
+        /// <summary>
+        /// Starting mouse click point
+        /// </summary>
+        private PointF startingPoint = PointF.Empty;
+        /// <summary>
+        /// Check is panning
+        /// </summary>
+        private bool panning = false;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public ResultForm()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="resultPlate">Result Plate</param>
+        public ResultForm(Plate resultPlate)
+        {
+            InitializeComponent();
+            result = resultPlate;
+        }
+
+        /// <summary>
+        /// Result form load handler
+        /// </summary>
+        /// <param name="sender">Sender</param>
+        /// <param name="e">Event arguments</param>
+        private void ResultForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Result picturebox paint
+        /// </summary>
+        /// <param name="sender">Sender</param>
+        /// <param name="e">Paint event arguments</param>
+        private void resultPicturebox_Paint(object sender, PaintEventArgs e)
+        {
+            graphic = new Graphic(e.Graphics);
+            graphic.DrawPlateResult(result);
+        }
+
+        /// <summary>
+        /// Call when mouse down on result picturebox
+        /// </summary>
+        /// <param name="sender">Sender</param>
+        /// <param name="e">Mouse event arguments</param>
+        private void resultPicturebox_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (ModifierKeys == Keys.Control && e.Button == MouseButtons.Left)
+            {
+                panning = true;
+                startingPoint = new PointF(e.Location.X, e.Location.Y);
+            }
+        }
+
+        /// <summary>
+        /// Call when mouse up on result picturebox
+        /// </summary>
+        /// <param name="sender">Sender</param>
+        /// <param name="e">Mouse event arguments</param>
+        private void resultPicturebox_MouseUp(object sender, MouseEventArgs e)
+        {
+            panning = false;
+        }
+
+        /// <summary>
+        /// Call when mouse move on result picturebox
+        /// </summary>
+        /// <param name="sender">Sender</param>
+        /// <param name="e">Mouse event arguments</param>
+        private void resultPicturebox_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (panning)
+            {
+                PointF movingVector = new PointF(e.Location.X - startingPoint.X, e.Location.Y - startingPoint.Y);
+                result.Move(movingVector);
+                startingPoint = new PointF(e.Location.X, e.Location.Y);
+                resultPicturebox.Refresh();
+            }
+        }
+
+        /// <summary>
+        /// Call when scroll mouse
+        /// </summary>
+        /// <param name="e">Mouse event</param>
+        protected override void OnMouseWheel(MouseEventArgs e)
+        {
+            base.OnMouseWheel(e);
+            resultPicturebox.Focus();
+            if (resultPicturebox.Focused == true && e.Delta != 0)
+            {
+                result.Zoom(e.Location, e.Delta > 0);
+                resultPicturebox.Refresh();
+            }
+        }
+
+        /// <summary>
+        /// Call when result picturebox size changed
+        /// </summary>
+        /// <param name="sender">Sender</param>
+        /// <param name="e">Event Arguments</param>
+        private void resultPicturebox_SizeChanged(object sender, EventArgs e)
+        {
+            resultPicturebox.Refresh();
+        }
+    }
+}
